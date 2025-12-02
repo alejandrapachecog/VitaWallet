c893a89c81c01544aa10902914b1f2460531bb6d
alejandrapacheco <alejandrapachecog@gmail.com>
2025-12-02 18:34:55 -0300

    tercer commit

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,6 +2,7 @@ package base;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -29,11 +30,48 @@ public class BaseTest {
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-
+        cerrarPopupChrome();
 
         cambiarAWebView();
         driver.get("https://stage.vitawallet.io/login");
     }
+
+    // ------------------------------------------------------------
+    // Cerrar popups nativos de Chrome automáticamente
+    // ------------------------------------------------------------
+    public void cerrarPopupChrome() {
+        try {
+            System.out.println("Intentando cerrar popup de Chrome...");
+
+            // Ir a contexto nativo
+            for (String ctx : driver.getContextHandles()) {
+                if (ctx.contains("NATIVE")) {
+                    driver.context(ctx);
+                }
+            }
+
+            Thread.sleep(800);
+
+            String[] posiblesIDs = {
+                    "com.android.chrome:id/terms_accept",
+                    "com.android.chrome:id/negative_button",
+                    "com.android.chrome:id/positive_button",
+                    "com.android.chrome:id/cancel_button"
+            };
+
+            for (String id : posiblesIDs) {
+                try {
+                    driver.findElement(By.id(id)).click();
+                    System.out.println("Popup cerrado con ID: " + id);
+                    break;
+                } catch (Exception ignored) {}
+            }
+
+        } catch (Exception e) {
+            System.out.println("Popup no encontrado (OK).");
+        }
+    }
+
 
     public void cambiarAWebView() {
         System.out.println("Contextos: " + driver.getContextHandles());
